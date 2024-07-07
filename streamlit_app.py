4d6de38157cfc6aa8e1bcbcc9889e4b58f867069
Paul <paulinkenbrandt@utah.gov>
2024-07-07 14:06:11 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,207 +7,126 @@ import folium
 from folium.plugins import Draw
 from folium import Popup
 from streamlit_folium import st_folium
-import os
-import plotly.express as px
-import glob
 import geopandas as gpd
-# from sqlalchemy import create_engine
-# import postgresql
-# import psycopg2
 
-# engine = create_engine(f"postgresql+psycopg2://{user}:{pw}@{ip}:5432/groundwater")
+# Remove redundant import
+# import plotly.express as px
+# import glob
 
-# Show the page title and description.
 # Initialize Streamlit app layout
-param_select = {'Air Temperature':[['TA_1_1_1','TA_1_4_1','TA_1_2_1','TA_1_3_1','T_SONIC',],
-                                "Temperature (°C)","Temperature Sensor",
-                                ],
-                'Soil Temperature':[['TS_1_1_1','TS_1_2_1','TS_3_1_1',],
-                                    "Temperature (°C)","Temperature Sensor",
-                                    ],
-                'Relative Humidity':[['RH_1_1_1','RH_1_2_1','RH_1_3_1',],
-                                    "Relative Humidity (%)","RH Sensor",
-                                    ],
-                'Energy Components':[['LE','H','G','NETRAD'],'Energy (W/m²)',
-                                    'Energy Component',
-                                    ]                   
-                }
+st.set_page_config(page_title="UFN", page_icon="🌎", layout="wide")
+st.title("Utah Geological Survey-Utah Flux Network")
+
+ufn_url = "https://geology.utah.gov/utah-flux-network"
+st.write(f"[The Utah Flux Network]({ufn_url})")
+
+param_select = {
+    'Air Temperature': [['TA_1_1_1', 'TA_1_4_1', 'TA_1_2_1', 'TA_1_3_1', 'T_SONIC'], "Temperature (°C)", "Temperature Sensor"],
+    'Soil Temperature': [['TS_1_1_1', 'TS_1_2_1', 'TS_3_1_1'], "Temperature (°C)", "Temperature Sensor"],
+    'Relative Humidity': [['RH_1_1_1', 'RH_1_2_1', 'RH_1_3_1'], "Relative Humidity (%)", "RH Sensor"],
+    'Energy Components': [['LE', 'H', 'G', 'NETRAD'], 'Energy (W/m²)', 'Energy Component']
+}
 
 @st.cache_data
-def stat_data():
+def load_data():
     df = pd.read_parquet('data/all_data.parquet')
     df.reset_index(inplace=True)
     df.replace(-9999, None, inplace=True)
     return df
 
 @st.cache_data
-def single_station(data, selected_site, sel_variables):
-    plotparam = []
-    for parm in sel_variables:
-        if parm in data.columns:
-            plotparam.append(parm)
-
+def filter_station_data(data, selected_site, sel_variables):
+    plotparam = [parm for parm in sel_variables if parm in data.columns]
     df = data.set_index(['datetime_start'])
-    df = df[df['station']==selected_site][plotparam]
-    #df['datetime_start'] = pd.to_datetime(['datetime_start'])
-    
-    return df,plotparam
+    df = df[df['station'] == selected_site][plotparam]
+    return df, plotparam
 
 @st.cache_data
-def three_hr(df):
-    df3 = df.resample('3h').mean()
-    return df3
+def resample_three_hours(df):
+    return df.resample('3h').mean()
 
-st.set_page_config(page_title="UFN", page_icon="🌎",
-                   layout="wide")
-st.title("Utah Geological Survey-Utah Flux Network")
+@st.cache_resource
+def create_violin_plot(data_df, param='Air Temperature'):
+    stat_temp = data_df.reset_index().melt(id_vars=['datetime_start'])
+    fig = px.violin(stat_temp, y="value", x="variable", box=True, hover_data=stat_temp.columns,
+                    labels={"value": param_select[param][1], "variable": param_select[param][2]},
+                    title=param)
+    return fig
 
-ufn_url = "https://geology.utah.gov/utah-flux-network"
-st.write(f"[The Utah Flux Network]({ufn_url})")
+@st.cache_resource
+def create_energy_balance_plot(df, selected_months):
+    df_energy = df[['G', 'LE', 'NETRAD', 'H']].dropna()
+    df_energy['Rn - G'] = df_energy['NETRAD'] - df_energy['G']
+    df_energy['LE + H'] = df_energy['H'] + df_energy['LE']
+    ebal = df_energy[df_energy.index.month_name().isin(selected_months)]
+    ebal = ebal[ebal.index.hour.isin(range(6, 21))]
+    fig = px.scatter(ebal, y="LE + H", x="Rn - G", trendline="ols")
+    return fig
 
-# Load the station data
+# Load data and station information
+data = load_data()
 stations_file_path = 'data/stations.geojson'
 sites = gpd.read_file(stations_file_path)
 sitelist = list(sites['Site ID'].values)
-data = stat_data()
 
-# Function to create a Folium map with markers
-def create_map(sts):
-    m = folium.Map(location=[sts.centroid.y.mean(),
-                        sts.centroid.x.mean()],
-            zoom_start=6)
-    folium.GeoJson(sts,
-                   marker=folium.Marker(icon=folium.Icon(icon='star')),
+def create_map(sites):
+    m = folium.Map(location=[sites.centroid.y.mean(), sites.centroid.x.mean()], zoom_start=6)
+    folium.GeoJson(sites, marker=folium.Marker(icon=folium.Icon(icon='star')), 
                    popup=folium.GeoJsonPopup(fields=["Site ID"])).add_to(m)
     return m
 
-# Function to get the site name from map click
 def get_site_name(map_data):
     if map_data['last_object_clicked']:
         lat = map_data['last_object_clicked']['lat']
-        site = sites[sites.geometry.y==lat]['Site ID'].values[0]
+        site = sites[sites.geometry.y == lat]['Site ID'].values[0]
         return site
     return None
 
 # Create a Folium map and display it in the sidebar
 st.sidebar.header("Map")
 folium_map = create_map(sites)
-# "with" notation
 with st.sidebar:
     map_data = st_folium(folium_map, width=300, height=500)
 
 amfluxurl = "https://ameriflux.lbl.gov/sites/siteinfo/"
-# Update dropdown based on map click
 clicked_site = get_site_name(map_data)
-if clicked_site:
-    st.write(f"You selected {clicked_site} from the map")
-    selected_site = clicked_site
-    selsite = clicked_site
-else:
-    selsite = 'US-UTB'
+selected_site = clicked_site if clicked_site else 'US-UTB'
 
-selected_site = st.selectbox("Select a site:", 
-                                options=sitelist,
-                                index = sitelist.index(selsite)
-                                )
+selected_site = st.selectbox("Select a site:", options=sitelist, index=sitelist.index(selected_site))
 
 if selected_site:
-    # Display additional information
     site_info = sites[sites['Site ID'] == selected_site]
     if not site_info.empty:
         snm = site_info['Name'].values[0]
         st.subheader(f"[{selected_site}: {snm}]({amfluxurl}{selected_site})")
     else:
         st.write("No additional information available for this site.")
-    vio_parm = st.selectbox("Parameter",
-            param_select.keys(), index=0
-            )
+    vio_parm = st.selectbox("Parameter", param_select.keys(), index=0)
 else:
     vio_param = 'Air Temperature'
+
 col1, col2 = st.columns(2)
 
 with col1:
-    # Main page dropdown
-
-    if selected_site:
-        # Display additional information
-
-        if selected_site in data.station.unique():  
-            sel_variables = param_select[vio_parm][0]
-            data_df, plotparam = single_station(data, selected_site,sel_variables)
-            # Filter data for the selected site
-            #site_data = data_df[data_df['station'] == selected_site]
-
-
-            # Plot the timeseries data
-            df3 = three_hr(data_df)
-            fig = px.line(df3, x=df3.index, y=plotparam, 
-                        title=f"{vio_parm} at {selected_site}")
-            st.plotly_chart(fig)
-
-
+    if selected_site in data['station'].unique():
+        sel_variables = param_select[vio_parm][0]
+        data_df, plotparam = filter_station_data(data, selected_site, sel_variables)
+        df3 = resample_three_hours(data_df)
+        fig = px.line(df3, x=df3.index, y=plotparam, title=f"{vio_parm} at {selected_site}")
+        st.plotly_chart(fig)
 
 with col2:
-    # Handle navigation to the detailed station page
-     
-    if selected_site:
-        # Display additional information
+    if selected_site in data['station'].unique():
+        molist = list(data_df.index.month_name().unique())
+        fig = create_violin_plot(data_df, vio_parm)
+        st.plotly_chart(fig)
+    else:
+        st.write("Select a point on the map to view data")
 
-        if selected_site in data.station.unique():  
+cont = st.container()
+allvals = st.checkbox("Select All", key=23)
+sel_opts = cont.multiselect("Select one or more options:", molist, molist if allvals else [], key=54)
 
-            molist = list(data_df.index.month_name().unique())
-            
-            container = {}
-            allp = {}
-            selected_options = {}
-            stat_temp = {}
-            tdf = {}
-            fig1 = {}
-
-            @st.cache_resource
-            def vio(cols, param= 'Air Temperature'):
-
-                stat_temp[param] = data_df.reset_index().melt(id_vars=['datetime_start'])
-
-
-                fig = px.violin(stat_temp[param], y="value", x="variable", 
-                                    box=True, hover_data=stat_temp[param].columns,
-                                    labels = {"value": param_select[param][1],
-                                            "variable":param_select[param][2] 
-                                            },
-                                            title=param
-                                    )
-                return fig
-
-
-                
-
-            @st.cache_resource
-            def ebalance(ddf, sel_opts):
-                df_energy =  ddf[['G','LE','NETRAD','H']].dropna()
-                df_energy['Rn - G'] = df_energy['NETRAD'] - df_energy['G']
-                df_energy['LE + H'] = df_energy['H'] + df_energy['LE']
-                ebal = df_energy[df_energy.index.month_name().isin(sel_opts)]
-                ebal = ebal[ebal.index.hour.isin(range(6,21))]
-                fig3 = px.scatter(ebal, y="LE + H", x="Rn - G", trendline="ols")
-                return fig3
-
-            fig = vio(plotparam, vio_parm)
-            st.plotly_chart(fig)
-
-            cont = st.container()
-            allvals = st.checkbox("Select All", key=23)
-        
-            if allvals:
-                sel_opts = cont.multiselect("Select one or more options:",
-                molist,molist, key=54)
-            else:
-                sel_opts =  cont.multiselect("Select one or more options:",
-                molist, key=54)
-
-            ebal_data, params = single_station(data,selected_site,['LE','H','G','NETRAD'])
-            fig3 = ebalance(ebal_data, sel_opts)
-            st.plotly_chart(fig3)
-
-        else:
-            st.write("Select a point on the map to view data")
+ebal_data, _ = filter_station_data(data, selected_site, ['LE', 'H', 'G', 'NETRAD'])
+fig3 = create_energy_balance_plot(ebal_data, sel_opts)
+st.plotly_chart(fig3)
